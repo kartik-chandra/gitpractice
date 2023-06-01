a0314866b000cd2577e8c4c80c86792675f21a47
onsaurav <kartik.chandra@wundermanthompson.com>
2023-06-01 20:48:42 +0600

    fixed Name of the player

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 console.log("This is the beginning!");
 console.log("LiveServer is enabled!");
+
+const players = ["Saurav Ganguly", "Sachin Tendulkar", "Rahul Deavid", "Arivind De Silva", "Brian Lara"];
+console.log(players);

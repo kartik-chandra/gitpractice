218c1f5242f294c6b9d2fb5d2832a7bfce504c53
onsaurav <kartik.chandra@wundermanthompson.com>
2023-06-01 21:09:58 +0600

    Update-kcb-test

--- js/index.js
+++ js/index.js
@@ -3,3 +3,12 @@ console.log("LiveServer is enabled!");
 
 const players = ["Saurav Ganguly", "Sachin Tendulkar", "Rahul Deavid", "Arivind De Silva", "Brian Lara"];
 console.log(players);
+
+const stat = [
+    {id: 1, name: "Saurav Ganguly", run: 13500, wicket: 100},
+    {id: 2, name: "Sachin Tendulkar", run: 25500, wicket: 200},
+    {id: 3, name: "Rahul Dravid", run: 17500, wicket: 10}
+];
+
+const allrounder = stat.filter(x=> (x.run > 10000 && x.wicket >= 100));
+console.log(allrounder);

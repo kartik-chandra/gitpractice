dca7dabc123a8bd0def16fe2e97336c0cc09bb93
onsaurav <kartik.chandra@wundermanthompson.com>
2023-06-09 10:43:37 +0600

    some updates

--- js/index.js
+++ js/index.js
@@ -19,3 +19,27 @@ console.log(playersRun);
 console.log(playersRun.shift());
 console.log(playersRun);
 
+console.log("This JavaScript array's depth is : " + playersRun.length);
+
+const totalRun = stat.reduce((runs, player)=>{
+    return runs + player.run;
+},0);
+
+console.log(`Total runs are : ${totalRun}`);
+
+const apps = [
+    { id: 1, name: "Marcombox", dev: 135, qa: 100 },
+    { id: 2, name: "Brandshare", dev: 255, qa: 200 },
+    { id: 3, name: "Miranda", dev: 175, qa: 10 },
+    { id: 4, name: "UserManagement", dev: 175, qa: 10 },
+    { id: 5, name: "Miranda", dev: 175, qa: 10 }
+];
+
+const slicedApp = apps.slice(2,4);
+console.log(slicedApp);
+
+const spApps = apps.find((x)=> x.dev == 175);
+console.log(spApps);
+
+console.log("Way of showing property : " + apps[0]["name"]);
+
